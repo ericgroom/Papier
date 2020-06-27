36149012344ced1eb55b413683685fd13f8f786b
Eric Groom <bcgroom@gmail.com>
2020-06-27 15:03:14 -0700

    Initial Commit

---
+++ Shared/PapierApp.swift
@@ -0,0 +1,17 @@
+//
+//  PapierApp.swift
+//  Shared
+//
+//  Created by Eric Groom on 6/27/20.
+//
+
+import SwiftUI
+
+@main
+struct PapierApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
